532a8df0f727e5cad0a03bceddf67ac227b4c2e8
Vishnu2400 <lvishnuvardhan886@gmail.com>
2025-03-19 14:38:00 -0500

    Initial commit

---
+++ ITMD544_SongLyric/src/main/java/com/vlingampally/ITMD544_SongLyric/SongLyricApplication.java
@@ -0,0 +1,13 @@
+package com.vlingampally.ITMD544_SongLyric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongLyricApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongLyricApplication.class, args);
+	}
+
+}
---
+++ ITMD544_SongLyric/src/test/java/com/vlingampally/ITMD544_SongLyric/SongLyricApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vlingampally.ITMD544_SongLyric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongLyricApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
